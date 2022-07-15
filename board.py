ab4871a652e6ab3a55068f186861d017d339eda2
Leighton Lauton <leighton.lauton@bendix.com.au>
2022-07-15 16:00:46 +1000

    created ai move and winning conditions checker
    now the AI can make a move
    begun creating the checkWinningConditions function to determine if a winner can be determined

--- board.py
+++ board.py
@@ -1,3 +1,5 @@
+import random
+
 class Board:
     size = 3
     tiles = [[], [], []]
@@ -24,9 +26,33 @@ class Board:
             print()
 
     def playMove(self, x, y, cur):
-        if (self.tiles[x][y] == "E"):
-            self.tiles[x][y] = cur
+        if (x > self.size) or (y > self.size):
+            print("invalid selection made")
+            return False
+        if (self.tiles[x - 1][y - 1] == "E"):
+            self.tiles[x - 1][y - 1] = cur
             print("valid move")
             self.draw()
+            return True
         else:
             print("invalid move made")
+            return False
+
+    def playAIMove(self):
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
+
+        if (self.tiles[row][col] == "X"):
+            self.playAIMove()
+        else:
+            self.tiles[row][col] = "O"
+        
+        print("Move made")
+        self.draw()
+
+    def checkWinningConditions(self):
+        print("Winner determined")
+
+        winningConditions = [[1, 2, 3], [4, 5, 6], [7, 8, 9],
+                             [1, 4, 7], [2, 5, 8], [3, 6, 9],
+                             [1, 5, 9], [3, 5, 7]]
--- game.py
+++ game.py
@@ -6,7 +6,14 @@ b.draw()
 ##b.playMove(0, 0, "X")
 ##b.playMove(0, 0, "O")
 
-inputX = int(input("What row would you like to play your move? "))
-inputY = int(input("What column would you like to play your move? "))
+print("If you would like to place a X in row 1, 1." + 
+      "For each of the following, type 1 respectively. A general rule is that row n, column n will be the nth spot on the grid.")
 
-b.playMove(inputX, inputY, "X")
+while (True):
+    inputX = int(input("What row would you like to play your move? "))
+    inputY = int(input("What column would you like to play your move? "))
+
+    b.playMove(inputX, inputY, "X")
+
+    print("Opponent playing move...")
+    b.playAIMove()
