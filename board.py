ac54b4df6b4565848be684f3aac819fce643c7c8
Leighton Lauton <leighton.lauton@bendix.com.au>
2022-07-15 15:41:11 +1000

    Player can place X on grid

--- board.py
+++ board.py
@@ -1,20 +1,32 @@
 class Board:
-    tiles = []
+    size = 3
+    tiles = [[], [], []]
     
     def __init__(self):
-        print("Board Initialized with size (8, 8)")
+        self.createBoard()
+        print("Board Initialized with size (3, 3)")
 
-    def createBoard(self):
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
-        self.tiles.append("x")
+    def createBoard(self):        
+        self.tiles[0].append("E")
+        self.tiles[0].append("E")
+        self.tiles[0].append("E")
+        self.tiles[1].append("E")
+        self.tiles[1].append("E")
+        self.tiles[1].append("E")
+        self.tiles[2].append("E")
+        self.tiles[2].append("E")
+        self.tiles[2].append("E")
 
     def draw(self):
-        for i in range(len(self.tiles)):
-            print("[]")
+        for i in self.tiles:
+            for j in i:
+                print(j, end = " ")
+            print()
+
+    def playMove(self, x, y, cur):
+        if (self.tiles[x][y] == "E"):
+            self.tiles[x][y] = cur
+            print("valid move")
+            self.draw()
+        else:
+            print("invalid move made")
--- game.py
+++ game.py
@@ -1,1 +1,12 @@
-print("hello world")
+from board import Board
+
+b = Board()
+b.draw()
+
+##b.playMove(0, 0, "X")
+##b.playMove(0, 0, "O")
+
+inputX = int(input("What row would you like to play your move? "))
+inputY = int(input("What column would you like to play your move? "))
+
+b.playMove(inputX, inputY, "X")
