a3e12ad832f5862b446904d4559bb01502cf4a10
Leighton Lauton <leighton.lauton@bendix.com.au>
2022-07-15 15:13:18 +1000

    Update board.py

--- board.py
+++ board.py
@@ -1,3 +1,20 @@
 class Board:
+    tiles = []
+    
     def __init__(self):
         print("Board Initialized with size (8, 8)")
+
+    def createBoard(self):
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+        self.tiles.append("x")
+
+    def draw(self):
+        for i in range(len(self.tiles)):
+            print("[]")
